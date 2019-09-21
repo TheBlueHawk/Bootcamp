b7fe9b43d057e3cf82590d5228e769340fbcb6d3
TheBlueHawk <kblin@outlook.com>
2019-09-21 16:14:10 +0200

    Created an independent repository for this week

---
+++ src/main/java/Bootcamp/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, SWENG");
+    System.out.println("The ultimate question of Life, The Universe, and Everything is: " + computeUltimateQuestion());
+  }
+
+  public static String computeUltimateQuestion() {
+    return "still searching...";
+  }
+
+  public static int computeUltimateAnswer() {
+    return 6 * 7;
+  }
+}
